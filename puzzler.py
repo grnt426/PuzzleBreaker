b63a5ca5cad3bf8ccc3ee8e4d56204e865061104
Grant Kurtz <kurtz.grant@gmail.com>
2016-01-07 21:05:13 -0500

    Changing over to a simpler match-3 puzzle.
    Also upgraded to a later version of Sikulix, with Python 2.7

--- puzzler.py
+++ puzzler.py
@@ -14,9 +14,12 @@ def getTileIndex(x, y):
     return index
 
 def findTilePositions(tileType, tileImg):
-    matches = puzRegion.findAll(tileImg)
+    parseMatches(puzRegion.findAll(tileImg), tileType)
+
+def parseMatches(matches, tileType):
     while matches.hasNext():
         tileData = matches.next()
+        #tileData.highlight()
         tileData = tileData.getTarget()
         tileIndex = getTileIndex(tileData.getX(), tileData.getY())
         prevValue = 0
@@ -26,11 +29,12 @@ def findTilePositions(tileType, tileImg):
             puzzleTiles[tileIndex[1]][tileType] = 0
         puzzleTiles[tileIndex[1]][tileType] = prevValue | (1 << tileIndex[0])
 
-print "A bot to play Puzzle Pirates!"
+print "A bot to play 3-Match!"
+print "Python version %s" % (sys.version)
 
 # Define the puzzle region
-windowTopLeft = find("topLeftCorner.png").getTarget()
-windowBottomRight = find("bottomRightCorner.png").getTarget()
+windowTopLeft = find("1452210177625.png").getTarget()
+windowBottomRight = find("1452210191657.png").getTarget()
 leftX = windowTopLeft.getX()
 leftY = windowTopLeft.getY()
 rightX = windowBottomRight.getX()
@@ -38,63 +42,58 @@ rightY = windowBottomRight.getY()
 width = rightX - leftX
 height = rightY - leftY
 puzRegion = Region(leftX, leftY, width, height)
+Settings.MinSimilarity = 0.95
 
 # Print some data about the puzzle
 print "=Puzzle Region=" 
 print "Pos: %d, %d" % (leftX, leftY)
 print "Bounds: %d, %d" % (width, height)
-tileW = width / 6
-tileH = height / 12
+tileW = width / 8
+tileH = height / 8
 print "=Tile Data="
 print "Size: %d, %d" % (tileW, tileH)
-tileTypes = {0: "blueSquare.png", 1: "blueCircle.png", 2: "whiteCircle.png", 3: "bigOctagon.png", 4: "smallOctagon.png"}
+tileTypes = {0: "1452210238386.png", 1: "1452210253817.png", 2: "1452210263737.png", 3: "1452211649479.png", 4: "1452210282937.png", 5: "1452210297009.png", 6: "1452210319985.png"}
 print tileTypes
 
 # Build the gamestate
 puzzleTiles = defaultdict(dict)
 for key in tileTypes:
     findTilePositions(key, tileTypes[key])
-print
 print "Puzzle State: "
 print puzzleTiles
 
-def convertBitMaskToCount(i):
-    i = i - ((i >> 1) & 0x55555555);
-    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-    return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
-
 def getPosOfSetBit(bit):
-    if bit == 1:
+    if bit == 0:
         return 0
-    elif bit == 2:
-        return 1
-    elif bit == 4:
-        return 2
-    elif bit == 8:
-        return 3
-    elif bit == 16:
-        return 4
-    elif bit == 32:
-        return 5
+    return int(math.log(bit)/math.log(2))
 
 matchFound = 0
+#while(matchFound == 1):
+ #   matchFound = 0
 for key in puzzleTiles:
     curRow = puzzleTiles[key]
     for tileType in curRow:
         marks = curRow[tileType]
         xox = marks & (marks << 2)
         xx = marks & (marks << 1)
-        if convertBitMaskToCount(marks) > 2 and (xox - xx) > 0:
+        binCount = bin(marks).count("1")
+        print "Old %d, New %d" % (getPosOfSetBit(marks), binCount)
+        if binCount >= 3 and getPosOfSetBit(xox) - getPosOfSetBit(xx) == 2:
+            if getPosOfSetBit(xx) == 0 and curRow[tileType] != key:
+                continue
             print "Key %d, Type %d, Val %d" % (key, tileType, marks)
             print "XOX %d, XX %d" % (xox, xx)
             col = getPosOfSetBit(xox >> 1)
+            print "Col %d" % (col)
             action = Region(col * tileW + leftX, key * tileH + leftY, tileW, tileH)
-            action.highlight(1)
+            action.highlight()
             action.setX(action.getX() + 5)
-            puzRegion.mouseMove(action)
-            puzRegion.mouseDown(Button.LEFT)
-            puzRegion.mouseUp(Button.LEFT)
+            #puzRegion.mouseMove(action)
+            #puzRegion.mouseDown(Button.LEFT)
+            #puzRegion.mouseUp(Button.LEFT)
             matchFound = 1
 
 if matchFound == 0:
     print "Found no matches, make me better :("
+
+time.sleep(5)
